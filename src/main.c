c14fe771bfcc5616cd4e41f654818151a763587a
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-02-21 12:15:15 +0100

    feat(main): add base main

--- src/main.c
+++ src/main.c
@@ -7,10 +7,26 @@
 
 #include "ftp.h"
 
+void help_flag(void)
+{
+    printf("USAGE: ./myftp port path\n");
+    printf("\tport is the port number on which the server socket listens\n");
+    printf("\tpath is the path to the home "
+    "directory for the Anonymous user\n");
+}
 
 int main(int ac, char **av)
 {
-    (void)ac;
-    (void)av;
+    server_t *server = NULL;
+
+    if (ac != 3) {
+        help_flag();
+        return 84;
+    }
+    server = init_server(strtol(av[1], NULL, 10), av[2]);
+    if (!server)
+        return 84;
+    if (ftp_server(server) == 84)
+        return 84;
     return 0;
 }
