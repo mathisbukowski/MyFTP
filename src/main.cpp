a913ca1181d8672882ef4e50b858e91a4839b981
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-03-11 15:32:31 +0100

    fix: error handling in main

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,25 @@
 #include "../include/ftp.hpp"
 #include "Server/Server.hpp"
 
-void help_flag()
+int help_flag()
 {
     std::cout << "USAGE: ./myftp port path" << std::endl;
     std::cout << "\tport\tis the port number on which the server socket listens" << std::endl;
     std::cout << "\tpath\tis the path to the home directory for the Anonymous user" << std::endl;
+    return 0;
 }
 
 int main(int ac, char **av)
 {
-    if (ac != 3) {
-        help_flag();
+    if (av[1] && strcmp("-help", av[1]) == 0)
+        return help_flag();
+    if (ac != 3)
         return 84;
-    }
-    ftp::Server server(strtol(av[1], NULL, 10), av[2]);
-    return server.process();
+    int port = std::stoi(av[1]);
+    if (port <= 0 || port > 65535)
+        return 84;
+    if (!std::filesystem::exists(av[2]))
+        return 84;
+    ftp::Server server(port, av[2]);
+    return server.run();
 }
